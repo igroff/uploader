412a92d1e6ed94a8d16532edc46a9f667ab148ee
Ian Groff <ian.groff@you_should_know.com>
2012-11-01 16:17:07 -0500

    more changes

--- src/server.py
+++ src/server.py
@@ -14,21 +14,14 @@ from flask import Response
 
 app = Flask(__name__)
 PORT = os.environ.get('FLASK_PORT', 5000)
-DEBUG = os.environ.get('FLASK_DEBUG', False)
 VERSION = os.environ.get('CURRENT_SHA', None)
-USE_RELOADER = DEBUG or os.environ.get('FLASK_RELOAD', False)
 
 @app.route("/diagnostic", methods=["GET"])
 def diagnostic_view():
     return jsonify(message="ok", version=VERSION)
 
-print("USE_RELOADER=%s" % USE_RELOADER)
-print("DEBUG=%s" % DEBUG)
-
 if (__name__ == "__main__"):
-    app.run(
-        debug=DEBUG,
-        use_debugger=DEBUG,
-        port=PORT,
-        use_reloader=USE_RELOADER
-    )
+    """ we should only get here for debugging, as we're gonna use gunicorn
+        for serving in production
+    """
+    app.run(use_reloader=True, debug=True, use_debugger=True, port=PORT)

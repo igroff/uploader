744e6ff31dca97cefeed27d8c31b19a253fd40a5
Ian Groff <ian.groff@you_should_know.com>
2013-02-05 09:18:02 -0600

    adding tests for the template rendering side of things

--- pyserver/handlers/template_render_handlers.py
+++ pyserver/handlers/template_render_handlers.py
@@ -3,7 +3,11 @@ from jinja2.exceptions import TemplateNotFound
 HTML_CONTENT_TYPE_HEADER = {"Content-Type": "text/html"}
 JSON_CONTENT_TYPE_HEADER = {"Content-Type": "application/json"}
 
-@app.route("/rend/<path:template_path>", methods=["GET", "POST"])
+@app.route("/template/<path:template_path>", methods=["POST"])
+def return_template_at(template_path):
+    pass
+
+@app.route("/render/<path:template_path>", methods=["GET", "POST"])
 def render_template_at(template_path):
     callback = request.values.get('callback', None)
 
@@ -21,10 +25,14 @@ def render_template_at(template_path):
         render_response = render_html_template(template_path, data)
 
     response_string, status, headers = render_response
+    # if we have been requested to use a callback, we want to render the
+    # response using the callbac, as well as setting the content type
+    # correctly
     if callback:
         # if the response is not already json, we'll make it so
         if not ('Content-Type', 'application/json') in set(headers.iteritems()):
             response_string = json.dumps(response_string)
+            headers['Content-Type'] = 'application/json'
         # wrap our json response string in the callback
         response_string = "%s(%s)" % (callback, response_string)
 
--- pyserver/tests/template_render_tests.py
+++ pyserver/tests/template_render_tests.py
@@ -1,8 +1,20 @@
+import os
+import json
 import uuid
 import unittest
 from pyserver import app
 from pyserver.core import *
 
+TEST_HELLO = """Hello, {{name}}"""
+TEST_HELLO_JSON = """{\"message\": "Hello, {{name}}"}"""
+TEST_TEMPLATES = [
+        dict(name="hello.html", content=TEST_HELLO),
+        dict(name="hello.json", content=TEST_HELLO_JSON),
+        ]
+
+for template in TEST_TEMPLATES:
+    with open(os.path.join("./templates", template['name']), 'w') as out_file:
+        out_file.write(template['content'])
 
 class TemplateRenderTestFixture(unittest.TestCase):
     def setUp(self):
@@ -18,16 +30,36 @@ class TemplateRenderTestFixture(unittest.TestCase):
         pass
 
     def test_html_template_with_data(self):
-        pass
+        r = self.app.post("/render/hello.html", data=dict(name="pants"))
+        self.assertEquals(200, r.status_code)
+        self.assertEquals("Hello, pants", r.data)
 
     def test_html_template_with_callback(self):
-        pass
+        request_data=dict(name="pants", callback="function_name")
+        r = self.app.post("/render/hello.html", data=request_data)
+        self.assertEquals(200, r.status_code)
+        self.assertEquals("application/json", r.content_type)
+        self.assertEquals("function_name(\"Hello, pants\")", r.data)
 
     def test_json_template_no_data(self):
-        pass 
+        r = self.app.post("/render/hello.json")
+        self.assertEquals(200, r.status_code)
+        jr = json.loads(r.data)
+        self.assertTrue(jr)
+        self.assertEquals("application/json", r.content_type)
+        self.assertEquals("Hello, ", jr['message'])
 
     def test_json_template_with_data(self):
-        pass
+        r = self.app.post("/render/hello.json", data=dict(name="pants"))
+        self.assertEquals(200, r.status_code)
+        jr = json.loads(r.data)
+        self.assertTrue(jr)
+        self.assertEquals("application/json", r.content_type)
+        self.assertEquals("Hello, pants", jr['message'])
 
     def test_json_template_with_callback(self):
-        pass
+        request_data=dict(name="pants", callback="function_name")
+        r = self.app.post("/render/hello.json", data=request_data)
+        self.assertEquals(200, r.status_code)
+        self.assertEquals("application/json", r.content_type)
+        self.assertEquals("function_name({\"message\": \"Hello, pants\"})", r.data)

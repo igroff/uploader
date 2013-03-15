0b593965af08640e8aad3333f3f48e1e8446978a
Ian Groff <ian.groff@you_should_know.com>
2013-03-15 10:07:04 -0500

    fixing a bunch of tests that were erroneously using assertTrue to use assertEqual

--- pyserver/tests/keyvalue_tests.py
+++ pyserver/tests/keyvalue_tests.py
@@ -1,8 +1,10 @@
 import unittest
 import json
+from uuid import uuid4
 import os
 from pyserver.core import *
 
+uuid = lambda: str(uuid4())
 
 class KVTestFixture(unittest.TestCase):
     def setUp(self):
@@ -20,45 +22,50 @@ class KVTestFixture(unittest.TestCase):
         self.assertEquals('p({"message": "no data for key"});', response.data)
 
     def test_odd_content_type(self):
-        response = self.app.post("/kv/my_key", data="some data", content_type="superpants")
-        self.assertTrue(200, response.status_code)
-        response = self.app.get("/kv/my_key")
-        self.assertTrue(200, response.status_code)
+        key_path = "/kv/%s" % (uuid())
+        response = self.app.post(key_path, data="some data", content_type="superpants")
+        self.assertEquals(200, response.status_code)
+        response = self.app.get(key_path)
+        self.assertEquals(200, response.status_code)
         self.assertEquals("some data", response.data)
         self.assertEquals("superpants", response.content_type)
 
     # testing some of the common content types that are handled by flask
     def test_app_javascript_content_type(self):
-        response = self.app.post("/kv/my_key", data="some data", content_type="application/javascript")
-        self.assertTrue(200, response.status_code)
-        response = self.app.get("/kv/my_key")
-        self.assertTrue(200, response.status_code)
+        key_path = "/kv/%s" % (uuid())
+        response = self.app.post(key_path, data="some data", content_type="application/javascript")
+        self.assertEquals(200, response.status_code)
+        response = self.app.get(key_path)
+        self.assertEquals(200, response.status_code)
         self.assertEquals("some data", response.data)
         self.assertEquals("application/javascript", response.content_type)
 
-    def test_app_json_content_type_invalid_json(self):
-        response = self.app.post("/kv/my_key", data="some data", content_type="application/json")
-        self.assertTrue(200, response.status_code)
-        response = self.app.get("/kv/my_key")
-        self.assertTrue(404, response.status_code)
+    def test_app_json_content_type_invalid_json_doesnt_store(self):
+        key_path = "/kv/%s" % (uuid())
+        response = self.app.post(key_path, data="some data", content_type="application/json")
+        self.assertEqual(400, response.status_code)
+        response = self.app.get(key_path)
+        self.assertEqual(404, response.status_code)
 
 
     def test_app_multipart_form_content_type_structured_data(self):
-        response = self.app.post("/kv/my_key", data=dict(pants="blue"))
-        self.assertTrue(200, response.status_code)
-        response = self.app.get("/kv/my_key")
-        self.assertTrue(200, response.status_code)
+        key_path = "/kv/%s" % (uuid())
+        response = self.app.post(key_path, data=dict(pants="blue"))
+        self.assertEqual(200, response.status_code)
+        response = self.app.get(key_path)
+        self.assertEqual(200, response.status_code)
         jr = json.loads(response.data)
         self.assertEquals("blue", jr['pants'])
         self.assertEquals("application/json", response.content_type)
 
     def test_create_from_request_body(self):
+        key_path = "/kv/%s" % (uuid())
         data = dict(one=1, two=2, name="pants")
-        response = self.app.post("/kv/my_key", data=json.dumps(data), content_type="application/javascript")
-        self.assertTrue(200, response.status_code)
+        response = self.app.post(key_path, data=json.dumps(data), content_type="application/javascript")
+        self.assertEqual(200, response.status_code)
         
-        response = self.app.get("/kv/my_key")
-        self.assertTrue(200, response.status_code)
+        response = self.app.get(key_path)
+        self.assertEqual(200, response.status_code)
         response_data = json.loads(response.data)
         self.assertEqual('application/javascript', response.content_type)
         self.assertEqual(1, response_data['one'], str(response_data))
@@ -66,11 +73,12 @@ class KVTestFixture(unittest.TestCase):
         self.assertEqual("pants", response_data['name'], str(response_data))
 
     def test_create_from_json(self):
+        key_path = "/kv/%s" % (uuid())
         data = dict(one=1, two=2, name="pants")
-        response = self.app.post("/kv/my_key", data=json.dumps(data), content_type="application/json")
-        self.assertTrue(200, response.status_code)
+        response = self.app.post(key_path, data=json.dumps(data), content_type="application/json")
+        self.assertEqual(200, response.status_code)
         
-        response = self.app.get("/kv/my_key")
+        response = self.app.get(key_path)
         self.assertTrue(200, response.status_code)
         response_data = json.loads(response.data)
         self.assertEqual(1, response_data['one'], str(response_data))
@@ -79,14 +87,12 @@ class KVTestFixture(unittest.TestCase):
 
     def test_delete(self):
         data = dict(one=1, two=2, name="pants")
-        response = self.app.post("/kv/my_key", data=data)
-        self.assertTrue(200, response.status_code)
-        response = self.app.get("/kv/my_key")
-        self.assertTrue(200, response.status_code)
-        response = self.app.delete("/kv/my_key")
-        self.assertTrue(200, response.status_code)
-        response = self.app.get("/kv/my_key")
-        self.assertTrue(404, response.status_code)
+        key_path = "/kv/%s" % (uuid())
+        self.assertEqual(404, self.app.get(key_path).status_code)
+        self.assertEqual(200, self.app.post(key_path).status_code)
+        self.assertEqual(200, self.app.get(key_path).status_code)
+        self.assertEqual(200, self.app.delete(key_path).status_code)
+        self.assertEqual(404, self.app.get(key_path).status_code)
 
     def test_concurrent(self):
         the_url_path = "/kv/myconcurrent_key"
